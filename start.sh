5a5f61025f6f873d3690c7d43cfeb8cc747846c7
Diegton Rodrigues <diegton@gmail.com>
2022-08-09 08:03:44 -0300

    copy ssh to user

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+mkdir ~/.ssh
+cp /root/.ssh/id_rsa ~/.ssh/id_rsa
+cp /root/.ssh/known_hosts ~/.ssh/known_hosts
 chmod 600 /root/.ssh/id_rsa
-echo "---"
+chmod 600 ~/.ssh/id_rsa
+echo "---#"
 ls -l /root/.ssh
 echo "---"
 git pull
